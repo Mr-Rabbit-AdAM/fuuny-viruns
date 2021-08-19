ed532ff05953326cb9df09fc03aedf73480ce03b
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-08-19 18:11:24 +0630

    Create virus.sh

---
+++ virus.sh
@@ -0,0 +1,52 @@
+#Do not take codes
+#By Ad A M
+
+notify-send 'Hacked By Hello Virus' 'I am Ad A M I love ma ma'
+
+
+
+xmessage -center "Hello, "
+
+xmessage -center " I am  Virus "
+
+zenity --warning --text="I have hacked your computer" --title="Hacked By Virus"
+
+zenity --error --text="Would you not believe it?" --title="hacked By Virus"
+
+zenity --info --text="I will open the calendar, so will you believe it?" --title="Hacked By Virus"
+
+zenity --calendar
+
+zenity --error --text="Do you believe?" --title="Hacked By Hello virus"
+
+clear 
+zenity --info --text="Let me ask you a question" --title="Hacked By Hello Virus"
+zenity --error --text="If my question is correct, I will return your computer" --title="Hacked By Hello Virus"
+zenity --error --text="If the answer is wrong, I'm going to blow up your computer" --title=" Hacked By Hello Virus"
+xmessage -button "Yes,No" "I think you will accept"
+zenity --error --text="Are you gay?" --title="Hacked By Hello Virus"
+trap '' 2
+echo "1 .yes "
+echo ""
+echo "2. No "
+echo ""
+read -p " Enter: " gh
+case $gh in
+1)
+
+
+zenity --info --text=" Are you really gay?" --title="Hakced By Hello"
+zenity --error --text="Because I'm not gay, I'm shut down your computer"
+zenity --error --text=" Ha ha " --title="Hacked By Hello "
+shutdown -h now
+;;
+
+2)
+zenity --error --text="You are right" --title="Hacked By Hello "
+zenity --error --text="I shut down your computer because you answered correctly" --title="Hacked By Hello"
+zenity --error --text=" Ha ha " --title="Hacked By Hello "
+shutdown -h now
+;;
+*)
+zenity --error --text="I shut down your computer because you made a wrong choice" --title="Haked By Hello"
+esac
